8b251b89d0849fc206d957d5909d3fbcbe7f613f
marestgaol <marcospg@icloud.com>
2026-06-22 17:50:55 +0200

    Sección 9 capítulo 7

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,66 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { PokemonModule } from './pokemon/pokemon.module';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { EnvConfiguration } from './config/app.config';
+
+import Joi from 'joi';
+import { JoiValidationSchema } from './config/joi.validation';
+
+import { join } from 'path';
+
+import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+
 import { SeedModule } from './seed/seed.module';
 
+/*
+Aquí me deja de funcionar la aplicación cuando hago un llamado al servidor Error:connect ECONNREFUSED 127.0.0.1:3000, el problema es que he creado una variable de entorno con PORT=3001 y daba conflicto porque tiene que haber un uso de esa variable
+*/
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            load: [EnvConfiguration],
+            validationSchema: JoiValidationSchema,
+        }),
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, '..', 'public'),
             renderPath: '/',
         }),
-        MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+        MongooseModule.forRoot(process.env.MONGODB!),
         PokemonModule,
         CommonModule,
         SeedModule],
 })
-export class AppModule { }
+export class AppModule {
+    constructor() {
+        // console.log(process.env)
+    }
+}
+
+/*
+Claude me da la solución: instalar joi
+*/
+// @Module({
+//     imports: [
+//         ConfigModule.forRoot({
+//             validationSchema: Joi.object({
+//                 MONGODB: Joi.string().required(),
+//                 PORT: Joi.number().default(3001),
+//             }),
+//         }),
+//         ServeStaticModule.forRoot({
+//             rootPath: join(__dirname, '..', 'public'),
+//             renderPath: '/',
+//         }),
+//         MongooseModule.forRoot(process.env.MONGODB!),
+//         PokemonModule,
+//         CommonModule,
+//         SeedModule],
+// })
+// export class AppModule {
+//     constructor() {
+//         console.log(process.env)
+//     }
+// }
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,3 +1,5 @@
+
+import { ConfigService } from '@nestjs/config';
 import { BadRequestException, ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 
 import { InjectModel } from '@nestjs/mongoose';
@@ -11,11 +13,17 @@ import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
+    private defaultLimit: number;
+
     // Contruir un modelo para inyectarlo en la base de datos y guardarlo
     constructor(
         @InjectModel(Pokemon.name)
-        private readonly pokemonModel: Model<Pokemon>
-    ) { }
+        private readonly pokemonModel: Model<Pokemon>,
+
+        private readonly configService: ConfigService,
+    ) {
+        this.defaultLimit = configService.get<number>('defaultLimit')!;
+    }
 
     async create(createPokemonDto: CreatePokemonDto) {
         createPokemonDto.name = createPokemonDto.name.toLowerCase();
@@ -73,7 +81,7 @@ export class PokemonService {
     }
 
     findAll(paginationDto: PaginationDto) {
-        const { limit = 10, offset = 0 } = paginationDto;
+        const { limit = this.defaultLimit, offset = 0 } = paginationDto;
 
         return this.pokemonModel.find()
             .limit(limit) // limite de resultados
